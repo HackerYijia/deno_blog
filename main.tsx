588577fac3c43aa77d5b3812c0d4ed8a2a1c5d93
诺影科技 NoxShadow Tech <115096818+HackerYijia@users.noreply.github.com>
2024-03-22 17:07:11 -0400

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,29 +1,14 @@
-import blog, { ga, redirects } from "https://deno.land/x/blog/blog.tsx";
-import { unocss_opts } from "./unocss.ts";
+import blog from "https://deno.land/x/blog/blog.tsx";
 
 blog({
   author: "诺影 NotJunar",
-  title: "诺影博客 | NotJunar Blog",
-  description: "12 岁寒，然后知松柏之后凋也。——《论语·子罕》",
-  avatar: "avatar.png",
+  title: "诺影的地下基地 | NotJunar's Blog",
+  description: "你将会点亮繁星，创造一个希望文明。",
+  avatar: "https://deno-avatar.deno.dev/avatar/83a531.svg",
   avatarClass: "rounded-full",
   links: [
     { title: "Discord", url: "dsc.gg/jnmc" },
     { title: "GitHub", url: "https://github.com/HackerYijia" },
-    { title: "Twitter", url: "https://twitter.com/NotJunar" },
   ],
-  lang: "en",
-  // localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
-  dateFormat: (date) =>
-    new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
-  middlewares: [
-    ga("UA-XXXXXXXX-X"),
-    redirects({
-      "/foo": "/my_post",
-      // you can skip leading slashes too
-      "bar": "my_post2",
-    }),
-  ],
-  unocss: unocss_opts, // check https://github.com/unocss/unocss
-  favicon: "favicon.ico",
+  lang: "zh",
 });
